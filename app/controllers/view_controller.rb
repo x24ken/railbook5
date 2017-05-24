f21f8fa9d116a71c8e93ac01c7ddb7262a01f8e0
x24ken <x24ken@gmail.com>
2017-05-24 02:47:46 +0000

    4.1.8

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -18,4 +18,11 @@ class ViewController < ApplicationController
   def select
     @book = Book.new(publish: '技術評論社')
   end
+  
+  def col_select
+    #フォームのもととなるモデルを準備
+    @book = Book.new(publish: '技術評論社')
+    #選択オプションの情報を取得
+    @books = Book.select(:publish).distinct
+  end
 end
