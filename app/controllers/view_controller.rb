6daad1da65d15aa7e7be9156011ae3fdbf283a55
x24ken <x24ken@gmail.com>
2017-05-24 02:38:22 +0000

    4.1.7

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -14,4 +14,8 @@ class ViewController < ApplicationController
   def html5
     @book = Book.new
   end
+  
+  def select
+    @book = Book.new(publish: '技術評論社')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   get 'view/form_for'
   get 'view/field'
   get 'view/html5'
+  get 'view/select'
 end
