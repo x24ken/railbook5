11327fc06ea83430a91872b71ee3bb3fe1cd11ff
x24ken <x24ken@gmail.com>
2017-05-28 07:10:48 +0000

    5.5.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :agreement, acceptance: true
+  validates :email,
+     presence: { unless: Proc.new { |u| u.dm.blank? } }
+  validates :agreement, acceptance: { on: :create }
   validates :email, confirmation: true
 end
